53118c7700cae41d3c94362b2fa98fae61419a48
vsidd <siddarth@ccs.neu.edu>
2015-03-12 04:55:25 -0400

    Added few comments

--- index/Indexer.java
+++ index/Indexer.java
@@ -29,7 +29,10 @@ import org.apache.commons.lang3.StringUtils;
 public class Indexer {
 
 	/**
-	 * @param args
+	 * This class will create my own index from the given location that will
+	 * replace the ElasticSearch calls.
+	 * This class will be able to handle large number of documents and terms
+	 * without using excessive memory or disk I/O.
 	 */
 	private static int termIndex = 1;
 	private static int lineBeginning = 0;
@@ -44,12 +47,16 @@ public class Indexer {
 	private static String invertedIndexFileLocation = "C:/College/temp/HW2-IR/src/InvertedIndex/";
 	private static String stopWordsFileLocation = "C:/College/IR/elasticsearch-1.4.2/config/stoplist.txt";
 
+	/**
+	 * The main function will handle most of the file operations. It will read the files in the 
+	 * given location and then it will send it to the parser which will index only 1000 documents 
+	 * at any given time
+	 */
 	public static void main(String[] args) {
 		Indexer indexer = new Indexer();
 		try {
 			URL url = Indexer.class.getResource("/resources/AP_DATA/ap89_collection/"); 
 			//URL url = Indexer.class.getResource("/resources/AP_DATA/test/"); 
-			//			System.out.println(url);
 			File stopWordsFile = new File(stopWordsFileLocation);
 			BufferedReader stopWordsReader = new BufferedReader(new FileReader(stopWordsFile));
 			String stopWordLine;
@@ -62,7 +69,6 @@ public class Indexer {
 				File folder;
 				folder = new File(url.toURI());
 				int fileNum = 1;
-				//				System.out.println("folder"+folder.length());
 				File[] listOfFiles = folder.listFiles();
 				for (int i = 0; i < listOfFiles.length; i++) {
 					File file = listOfFiles[i];
@@ -76,7 +82,6 @@ public class Indexer {
 			}
 
 
-			//			System.out.println(url);
 			File termIdMapFile = new File(otherFilesUrl+"termIdMap.txt");
 			File termTTFFile = new File(otherFilesUrl+"termTTF.txt");
 			File docIdMapFile = new File(otherFilesUrl+"docIdMap.txt");
@@ -140,7 +145,6 @@ public class Indexer {
 		}catch (URISyntaxException e) {
 			e.printStackTrace();
 		}catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -184,7 +188,6 @@ public class Indexer {
 						if(flag == 1000){
 							System.out.println("Creating inverted index for 1000 documents set : "+(someCounter++));
 							termOffsetMap = createInvertedIndex(termOffsetMap, documents);
-							//							System.out.println("Inverted index created");
 							documents.setLength(0);
 							flag = 0;
 						}
@@ -213,10 +216,6 @@ public class Indexer {
 		try{
 			System.out.println("Creating the catalog and final index files");
 			long indexLineOffset = 0;
-			//			File invertedIndexFile = new File(invertedIndexFileLocation+"invertedIndex.txt");
-			//			if(!invertedIndexFile.exists()){
-			//				throw new FileNotFoundException("Inverted index not found");
-			//			}
 
 			RandomAccessFile raf = new RandomAccessFile(invertedIndexFileLocation+"invertedIndex.txt", "r");
 
@@ -235,7 +234,6 @@ public class Indexer {
 			BufferedWriter addToCatalog = new BufferedWriter(fw2) ;
 
 			StringBuilder record = new StringBuilder();
-			String holder;
 			for(Map.Entry<String, ArrayList<Integer>> termOffset : termOffsetMap.entrySet()){
 				//				record.append(termOffset.getKey()+":");
 				for(int offset : termOffset.getValue()){
@@ -281,10 +279,7 @@ public class Indexer {
 							termDocBlock.get(tuple[0]).put(tuple[1], dBlock);
 						}
 					}else{
-						//						System.out.println(tuple[0]);   //termid, docid, position
-						//						System.out.println(tuple[1]);
-						//						System.out.println(tuple[2]);
-						//						System.out.println(termTTF.get(tuple[0]));
+						//termid, docid, position
 						DBlock dBlock = new DBlock(tuple[1], 1, termTTF.get(tuple[0]), tuple[2]);
 						LinkedHashMap<String, DBlock> docDblockMap = new LinkedHashMap<String, DBlock>();
 						docDblockMap.put(tuple[1], dBlock);
@@ -301,7 +296,6 @@ public class Indexer {
 
 	private Map<String, ArrayList<Integer>> createTermDblockFile(Map<String, ArrayList<Integer>> termOffsetMap,	LinkedHashMap<String, LinkedHashMap<String, DBlock>> termDocBlock) {
 		try{
-			//			System.out.println("Creating inverted index");
 			File invertedIndexFile = new File(invertedIndexFileLocation+"invertedIndex.txt");
 			if(!invertedIndexFile.exists()){
 				invertedIndexFile.createNewFile();
@@ -334,129 +328,6 @@ public class Indexer {
 		return termOffsetMap;
 	}
 
-	//	public void createTermDblockFile(Map<Map.Entry<String, String>, DBlock> termDblock){
-	//		try{
-	//			// file setup for inverted index
-	//			File invertedIndexFile = new File(invertedIndexFileLocation+"invertedIndex.txt");
-	//			//			boolean firstTime = false;
-	//			if(!invertedIndexFile.exists()){
-	//				invertedIndexFile.createNewFile();
-	//				//				firstTime = true;
-	//			}
-	//			FileWriter fw = new FileWriter(invertedIndexFile.getAbsoluteFile(), true);
-	//			BufferedWriter addToIndex = new BufferedWriter(fw) ;
-	//
-	//			// file setup for catalog
-	//			File catalogFile = new File(invertedIndexFileLocation+"catalog.txt");
-	//			if(!catalogFile.exists()){
-	//				catalogFile.createNewFile();
-	//			}
-	//			FileWriter fw2 = new FileWriter(catalogFile.getAbsoluteFile(), true);
-	//			BufferedWriter addToCatalog = new BufferedWriter(fw2) ;
-	//
-	//			String prev;
-	//			StringBuilder content = new StringBuilder();
-	//			//get first term separately 
-	//			Iterator<Map.Entry<Map.Entry<String, String>, DBlock>> iterator = termDblock.entrySet().iterator();
-	//			//			System.out.println("IM OUTSIDE");
-	//			if(iterator.hasNext()){
-	//				//				System.out.println("IM INSIDE");
-	//				Map.Entry<Map.Entry<String, String>, DBlock> temp = iterator.next();
-	//				prev = temp.getKey().getKey();
-	//				content.append(String.valueOf(prev+" : "));
-	//				//***********************************************************
-	//				for(Map.Entry<Map.Entry<String, String>, DBlock> termBlock : termDblock.entrySet()){
-	//					String term = termBlock.getKey().getKey();
-	//					String current = term;
-	//					//					System.out.println("prev : "+prev+" current : "+current);
-	//					if(!prev.equals(current)){
-	//						addToIndex.append(content);
-	//						//						System.out.println("content : "+content);
-	//						content.setLength(0);
-	//						content.append("\n"+term+" : ");
-	//						addToCatalog.append(prev+" "+(invertedIndexFile.length()-1)+"\n");
-	//					}
-	//					DBlock dBlock = termBlock.getValue();
-	//					content.append(dBlock.getDocID()+" ");
-	//					content.append(String.valueOf(dBlock.getTF()+" "));
-	//					content.append(String.valueOf(dBlock.getTTF()+" "));
-	//					content.append(dBlock.getPositions().toString()+"#");
-	//					prev = current;
-	//				}
-	//				addToIndex.append(content);
-	//				content.setLength(0);
-	//				addToCatalog.append(prev+" "+(invertedIndexFile.length()-1)+"\n");
-	//				addToCatalog.close();
-	//				addToIndex.close();
-	//				fw.close();
-	//				fw2.close();
-	//			}
-	//		}catch(IOException e){
-	//			e.printStackTrace();
-	//		}
-	//	}
-
-
-
-
-	//	public Map<Map.Entry<String, String>, DBlock> createInvertedIndex(Map<String, Integer> tempMap){
-	//		Map<Map.Entry<String, String>, DBlock> termDBlockMap = new LinkedHashMap<Map.Entry<String, String>, DBlock>();
-	//		try{
-	//			File tokenDirectory = new File(localUrl);
-	//			StringBuilder positions = new StringBuilder();
-	//			int termID;
-	//			int tf;
-	//			String docID;
-	//			LinkedHashMap<String, String> termDocMap = new LinkedHashMap<String, String>();
-	//						int tcount = 1;
-	//			File[] files = tokenDirectory.listFiles();
-	//			for(Map.Entry<String, Integer> termIdMapContent : tempMap.entrySet()){  // per term
-	//								System.out.println("term count : "+tcount);
-	//								tcount++;
-	//				for (int i = 0; i < files.length; i++) {                            // loop over all files
-	//					File file = files[i];
-	//					if(file.isFile())
-	//					{
-	//						BufferedReader br = new BufferedReader(new FileReader(file));
-	//						String currentLine;
-	//						while((currentLine = br.readLine())!= null){
-	//							//							System.out.println("CURRENT LINE : "+currentLine);
-	//							termID = termIdMapContent.getValue();
-	//							String[] tuples = currentLine.split("#");
-	//							tf = 0;
-	//							docID ="";
-	//							for(int j = 0; j<tuples.length;j++){
-	//								if(tuples[j].length() >1){
-	//									String[] tuple = currentLine.split(" ");
-	//									//									System.out.println("termID : "+termID+" tuple[0] :"+tuple[0]);
-	//									docID = tuple[1];
-	//									if(tuple[0].equals(String.valueOf(termID))){                     // optimize here
-	//										tf++;
-	////										System.out.println("TF : "+tf);
-	//										positions.append(tuple[2]+" ");
-	//									}
-	//								}
-	//							}
-	//							if(tf>0){
-	////								LinkedHashMap<String, String> termDocMap = new LinkedHashMap<String, String>(); // will LinkedHashMap obj good for LinkedHashMap.put() call
-	//								termDocMap.put(String.valueOf(termIdMapContent.getValue()), docID);
-	//								DBlock dBlock = new DBlock(docID, tf,termTTF.get(termIdMapContent.getValue()), positions);
-	//								positions.setLength(0);
-	//								for(Map.Entry<String, String> termDocMapTemp : termDocMap.entrySet()){
-	//									termDBlockMap.put(termDocMapTemp, dBlock);
-	//									termDBlockMap.clear();
-	//									break;
-	//								}
-	//							}
-	//						}
-	//					}
-	//				}
-	//			}
-	//		}catch(IOException e){
-	//			e.printStackTrace();
-	//		}
-	//		return termDBlockMap;
-	//	}
 
 
 
@@ -491,8 +362,6 @@ public class Indexer {
 					}
 				}
 				if(currentLine.contains("</DOC>")){
-					//					Document document = new Document(docID.trim(), sb);
-					//					docList.add(document);
 					indexer.generateTokens(sb.toString(), bw);
 					sb.setLength(0);
 				}
@@ -536,10 +405,6 @@ public class Indexer {
 								bw.append(content);
 								content.setLength(0);
 								positionID++;
-								//					int startIndex = matcher.start();
-								//					int endIndex = matcher.end();
-								//					System.out.println(termIndex + " " + term + " " + startIndex
-								//							+ " " + endIndex);
 							}
 						}
 					}
@@ -566,13 +431,6 @@ public class Indexer {
 			this.ttf = 0;
 			this.positions = new StringBuilder();
 		}
-
-		//		DBlock(String docID, int tf, int ttf, StringBuilder positions){
-		//			this.docID = docID;
-		//			this.tf += 1;
-		//			this.ttf = ttf;
-		//			this.positions = positions;
-		//		}
 
 		DBlock(String docID, int tf, int ttf, String position){
 			this.docID = docID;
@@ -616,31 +474,3 @@ public class Indexer {
 
 
 
-//public Map<String, DBlock> createInvertedIndex(Map<String, DBlock> tempMap){
-//try{
-//File tokenDirectory = new File(localUrl);
-//File[] files = tokenDirectory.listFiles();
-//for(int i = 0; i< files.length; i++){
-//	File file = files[i];
-//	if(file.isFile()){
-//		BufferedReader br = new BufferedReader(new FileReader(file));
-//		String currentLine;
-//		while((currentLine = br.readLine())!= null){
-//			String[] tuples = currentLine.split("#");
-//			for(int j = 0; j<tuples.length; j++){
-//				if(tuples[j].length() > 1){
-//					String[] tuple = tuples[j].split(" ");
-//					if(tempMap.containsKey(tuple[0])){
-//						
-//					}
-//				}
-//			}
-//		}
-//	}
-//}
-//}catch(IOException e){
-//e.printStackTrace();
-//}
-//return null;
-//}
-//
