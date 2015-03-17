63a4041f223fc23307fbd6ff29c79061fd9b1312
vsidd <siddarth@ccs.neu.edu>
2015-03-17 02:30:48 -0400

    Added comments

--- models/ParseAndRetrieve.py
+++ models/ParseAndRetrieve.py
@@ -2,6 +2,13 @@
 Created on Jan 28, 2015
 
 @author: Siddarthan
+
+This class evaluates the various retrieval models like
+Okapi-TF, Okapi BM25, TF-IDF, Unigram LM with Laplace Smoothing
+and Unigram LM with Jelinek-Mercer smoothing. 
+Initially this class worked with ElasticSearch tool to index documents 
+which are then commented out and uses my own interface to index and 
+read the required measurements
 '''
 import glob
 import os
@@ -14,17 +21,23 @@ import re
 from PorterStemmer import *
 import operator
 import ast
+from HW1.sidSearch import SidSearch
 
 
 #counter = 0
 def main():
     es = Elasticsearch()
 #    createIndex(es)
-    indexedDocuments = es.search(index='ap_dataset',body={"query": {"match_all": {}}}, size=84679)
+    search = SidSearch()
+    #print search.matchAll()
+    #indexedDocuments = es.search(index='ap_dataset',body={"query": {"match_all": {}}}, size=84679)
+    indexedDocuments = search.matchAll()
     docIDs = [doc['_id'] for doc in indexedDocuments['hits']['hits']]
+    print "here"
+    #print "docIDS : ",docIDs
     #docFreqAndAvg = avgDocLen(es , docIDs)
     avg = 247.806315615
-    totalDocuments = 84769.0
+    totalDocuments = 84678.0
     #avg = docFreqAndAvg[0]
     #docsLen = docFreqAndAvg[1]
     docLenFile = os.path.join(os.path.dirname(__file__), '/College/temp/HW1-IR/HW1/docLength.txt')
@@ -50,7 +63,7 @@ def main():
     queryCounter = -1
           
           
-    es.put_script(lang='groovy', id='getTF', body={"script": "_index[field][term].tf()"})
+    #es.put_script(lang='groovy', id='getTF', body={"script": "_index[field][term].tf()"})
             
     for currentLine in iter(f):
         queryCounter = queryCounter+1
@@ -68,10 +81,13 @@ def main():
             laplace_tracker = 1
             processedText = processText(currentLine)
             print processedText
+            proximitySearch(search, processedText, queryCounter, queryNo)
             for term in processedText.split():
-                scriptResult = es.search(index='ap_dataset',body={"query": {"function_score": {"query": {"match": {"text": term}},"functions": [{"script_score": {"script_id": "getTF","lang" : "groovy","params": {"term": term,"field": "text"}}}],"boost_mode": "replace"}},"size": totalDocuments,"fields": ["docno"]})
+                #scriptResult = es.search(index='ap_dataset',body={"query": {"function_score": {"query": {"match": {"text": term}},"functions": [{"script_score": {"script_id": "getTF","lang" : "groovy","params": {"term": term,"field": "text"}}}],"boost_mode": "replace"}},"size": totalDocuments,"fields": ["docno"]})
+                scriptResult = search.getTF(term)
                 df = scriptResult['hits']['total']
                 processedScriptResult = processScriptResult(scriptResult)
+                #print "script result : ",processedScriptResult
                 for x in range(len(processedScriptResult)):
                     if(lm_laplace_tracker.has_key(processedScriptResult[x][0]) == False):
                         lm_laplace_tracker[processedScriptResult[x][0]] = 1
@@ -135,21 +151,43 @@ def main():
         sorted_Jm_Smoothing = sorted(jm_smoothing.items(), key=operator.itemgetter(1), reverse = True)[:1000]
         
                      
-        writeOutputToFile(es, '1_Okapi_Result.txt', sorted_Okapi, queryCounter, queryNo)
-        writeOutputToFile(es, '2_TFIDF_Result.txt', sorted_tfIDF, queryCounter, queryNo)
-        writeOutputToFile(es, '3_OkapiBM25_Result.txt', sorted_OkapiBM25, queryCounter, queryNo)
-        writeOutputToFile(es, '4_LM_Laplace_Result.txt', sorted_Lm_Laplace, queryCounter, queryNo)
-        writeOutputToFile(es, '5_JM_Smoothing_Result.txt', sorted_Jm_Smoothing, queryCounter, queryNo)
+#         writeOutputToFile(es, '1_Okapi_Result.txt', sorted_Okapi, queryCounter, queryNo)
+#         writeOutputToFile(es, '2_TFIDF_Result.txt', sorted_tfIDF, queryCounter, queryNo)
+#         writeOutputToFile(es, '3_OkapiBM25_Result.txt', sorted_OkapiBM25, queryCounter, queryNo)
+#         writeOutputToFile(es, '4_LM_Laplace_Result.txt', sorted_Lm_Laplace, queryCounter, queryNo)
+#         writeOutputToFile(es, '5_JM_Smoothing_Result.txt', sorted_Jm_Smoothing, queryCounter, queryNo)
+#         
+        writeOutputToFile(search, '1_Okapi_Result.txt', sorted_Okapi, queryCounter, queryNo)
+        writeOutputToFile(search, '2_TFIDF_Result.txt', sorted_tfIDF, queryCounter, queryNo)
+        writeOutputToFile(search, '3_OkapiBM25_Result.txt', sorted_OkapiBM25, queryCounter, queryNo)
+        writeOutputToFile(search, '4_LM_Laplace_Result.txt', sorted_Lm_Laplace, queryCounter, queryNo)
+        writeOutputToFile(search, '5_JM_Smoothing_Result.txt', sorted_Jm_Smoothing, queryCounter, queryNo)
                  
-    
+
+"""
+Returns a list of tuples that contains (document_ID, term_frequrency, document_number)
+"""
 def processScriptResult(searchResult):
     docIDs = [(doc['_id'], doc['_score'], doc['fields']['docno'])for doc in searchResult['hits']['hits']]
     return docIDs
 
+"""
+Returns a list of tuples that contain (document_id, term, term_positions)
+"""
+def getPositions(term, searchResult):
+    docIDs = [(doc['_id'],term, doc['positions'])for doc in searchResult['hits']['hits']]
+    return docIDs
+
+"""
+Returns a list of tuples that contains (document_id, document_frequency, document_number)
+"""
 def processDFScriptResult(searchResult):
     docIDs = [(doc['_id'], doc['_score'], doc['fields']['docno'])for doc in searchResult['hits']['hits']]
     return docIDs
-         
+
+"""
+Returns a calculated value of TF-IDF from the given paramenters
+"""
 def tfidfCalculate(okapiResult, totalDocs, docFreq, term, id):
     if(docFreq != []):
         tfidf = okapiResult[2] * math.log(totalDocs/docFreq)
@@ -157,6 +195,9 @@ def tfidfCalculate(okapiResult, totalDocs, docFreq, term, id):
     else:
         return (-1, term, 0)
 
+"""
+Returns a calculated value of Okapi-BM25 from the given paramenters
+"""
 def okapiBM25Cal(D, df, tf, docLen, avg, tfwq):
     k1 = 1.2
     k2 = 100.0
@@ -167,11 +208,17 @@ def okapiBM25Cal(D, df, tf, docLen, avg, tfwq):
     bm25 = t1 * t2 * t3
     return bm25
 
+"""
+Returns a calculated value of Unigram LM with Laplace smoothing from the given paramenters
+"""
 def p_LaplaceCalc(tf, docLen):
     vocabSize = 178050
     p_Laplace = (tf+1.0)/((docLen+vocabSize)*1.0)
     return p_Laplace
 
+"""
+Returns a calculated value of Unigram LM with Jelinek_Mercer smoothing from the given paramenters
+"""
 def jelinek_mercerCalc(tf, docLen, resultTTF, resultTDF):
     smoother = 0.7
     t1 = smoother * (tf/docLen*1.0)
@@ -179,16 +226,96 @@ def jelinek_mercerCalc(tf, docLen, resultTTF, resultTDF):
     jelinek_mercer = t1 + t2
     return jelinek_mercer
 
+"""
+Returns a calculated value of Proximity search from the given paramenters
+"""
+def proximitySearch(search, processedText, queryCounter, queryNo):
+    docLenMap = {}
+    docLenMapFileLocation = os.path.join(os.path.dirname(__file__), '/College/temp/HW2-IR/src/OtherFiles/docLenMap.txt')
+    with open(docLenMapFileLocation) as docLenMapFile:
+        for line in docLenMapFile:
+            l = line.split(" ")
+            if(len(l) > 1):
+                docLenMap[l[0]] = l[1]
+                    
+    listOfPositions = []
+    for term in processedText.split():
+        positions = search.getTF(term)
+        #print"positions : ",getPositions(positions)
+        tdp = getPositions(term,positions)
+        if (len(tdp) == 0):
+            return 
+        listOfPositions += tdp
+    listOfPositions.sort()
+    docTermMap = {}
+    docIds = []
+    for block in listOfPositions:
+        if not block[0] in docTermMap:
+            docIds.append(block[0])
+            docTermMap[block[0]] = [(block[1],block[2])]
+        else:
+            docTermMap[block[0]].append((block[1],block[2]))
+    #print len(docTermMap['1'])
+    proximityScore={}
+    for id in docIds:
+        numberOfTerms = len(docTermMap[id])
+#         if(numberOfTerms == len(processedText.split())):
+        #print "ID : ",docTermMap[id]
+        termPointers = [0] * numberOfTerms
+        range = calculateRange(docTermMap[id], termPointers)
+        score = ((3000 - range) * numberOfTerms *1.0)/(1.0* int(docLenMap[id]) + 160470)
+        #score = ((range - numberOfTerms)*1.0)/(numberOfTerms * 1.0)
+        proximityScore[id] = score
+    sorted_Proximity = sorted(proximityScore.items(), key=operator.itemgetter(1), reverse = True)[:1000]
+    writeOutputToFile(search, '6_ProximitySearch_Result.txt', sorted_Proximity, queryCounter, queryNo)
+    #print processedText, "DDDDDDDDOOOOOOOOOOOONNNNNNNNNNNEEEEEEEEEEE"   
+
+def calculateRange(termPositions, termPointers):
+    positions =[]
+    for i in range(len(termPointers)):
+        val = termPositions[i][1]
+        intPositions = map(int, val)
+        positions.append(intPositions)
+        termPointers[i] = -len(val)
+    flag = True    
+    prev = 100000
+    #print "termPointers ",termPointers
+    while(flag):
+        flag = False
+        numbers = []
+        for i in range(len(termPointers)):
+            numbers.append(positions[i][termPointers[i]])
+        #print numbers
+        rang = max(numbers) - min(numbers)
+        if rang < prev:
+            prev = rang
+        
+        for i in range(len(termPointers)):
+            index = numbers.index(min(numbers))
+            if termPointers[index] != -1:
+                termPointers[index] += 1
+                flag = True
+                break
+    return prev
+
+"""
+Writes the result to a corresponding output file
+"""
 def writeOutputToFile(es, fileName, results, queryIndex, queryNumber):
     f = open(fileName, 'a')
     rank = 1
     for i in range(len(results)):
-        documentNumbers = es.get(index='ap_dataset', id=str(results[i][0]), doc_type='document')
+        #documentNumbers = es.get(index='ap_dataset', id=str(results[i][0]), doc_type='document')
+        #print "@@@@@@@@@@",documentNumbers
+        documentNumbers = es.getDocID(results[i][0]) 
         outputToWrite = str(queryNumber[queryIndex])+" "+"Q0 "+str(documentNumbers['_source']['docno'])+" "+str(rank)+" "+str(results[i][1])+" Exp\n"
         f.write(outputToWrite)
         rank += 1
     f.close()
-        
+    
+"""
+The query is processed to remove stop words and punctuations and also stem the query
+"""
 def processText(currentLine):
     currentLine = re.sub('[,"\']','',currentLine)
     currentLine = currentLine.replace("-"," ")
@@ -196,11 +323,16 @@ def processText(currentLine):
     currentLine = currentLine[4:]
     currentLine = removeWords(3, currentLine)
     currentLine = removeStopWords(currentLine)
-    currentLine = currentLine[:currentLine.rfind('.')]
+    if(currentLine.rfind('.') != -1):
+        currentLine = currentLine[:currentLine.rfind('.')]
     currentLine = stemQuery(currentLine)
     currentLine = currentLine.strip()
+    currentLine = currentLine.lower()
     return currentLine
 
+"""
+Stem the input query from given text
+"""
 def stemQuery(currentLine):
     temp = ""
     res = PorterStemmer()
@@ -208,6 +340,9 @@ def stemQuery(currentLine):
         temp = temp +" "+ res.stem(word, 0 , (len(word)-1))
     return temp
 
+"""
+Remove the stop words from given text
+"""
 def removeStopWords(currentLine):
     filename = os.path.join(os.path.dirname(__file__), '/College/IR/elasticsearch-1.4.2/config/stoplist.txt')
     f = open(filename, "r")
